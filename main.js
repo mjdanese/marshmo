ec27768022a4007238b88ad1056064cd0b488192
Marshall Danese <marshalldanese@gmail.com>
2021-09-14 21:12:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ Vue.component('navtop', {
     <h1><a class='titleRef' href='index.html'>mdanese</a></h1><br>
     <button @click='sendUpdate' class="btn btn-dark btn-sm">Random</button>
     <a href="/archive" class="btn btn-dark btn-sm">Archive</a>
-    <a href="/2021-09-14" class="btn btn-dark btn-sm">2021-09-14</a>
+    <a href="/2021-09-14" class="btn btn-dark btn-sm">2021-09-14 / Cat</a>
     <br><br>
   </div>
 
